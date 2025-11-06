3b1d70cb734301d6400e38285435b8c3f8de2479
Oluwadare Titus olamide <horlly582@gmail.com>
2025-11-06 14:29:14 +0100

    DONE

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import WelcomeMessage from "./components/WelcomeMessage";
-import Counter from "./components/counter";
+import Counter from "./components/Counter";
 import "./App.css";
 
 function App() {
