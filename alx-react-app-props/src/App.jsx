3813e3006402c775a23c8da6d267c8cd6fbdf825
Oluwadare Titus olamide <horlly582@gmail.com>
2025-11-07 10:20:24 +0100

    okk

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -13,7 +13,9 @@ function App() {
       <WelcomeMessage />
       <MainContent />
       <Footer />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <UserContext.Provider>
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      </UserContext.Provider>
     </>
   );
 }
