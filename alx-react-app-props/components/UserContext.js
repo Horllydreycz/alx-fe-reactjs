e2a9b84eda00f08c52216cb98876af83a7ef3133
Oluwadare Titus olamide <horlly582@gmail.com>
2025-11-07 07:47:49 +0100

    hh

--- alx-react-app-props/components/UserContext.js
+++ alx-react-app-props/components/UserContext.js
@@ -1,4 +1,4 @@
 import React from "react";
 import { useContext } from "react";
-const UserContext = React.createContext(undefined);
+const UserContext = React.createContext();
 export default UserContext;
