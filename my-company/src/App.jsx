99ccdd59662101b5f6345e5ceb9d7e265c80dc83
Oluwadare Titus olamide <horlly582@gmail.com>
2025-11-10 00:15:55 +0100

    fixed

---
+++ my-company/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
