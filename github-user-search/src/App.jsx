67e176d4410033c9df42b79fc5ef201d8ef1cf9b
Oluwadare Titus olamide <horlly582@gmail.com>
2025-11-23 19:09:17 +0100

    fixwd

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
 import About from "./components/About";
-import "../src/";
+import "./index.css";
 
 function App() {
   return (
