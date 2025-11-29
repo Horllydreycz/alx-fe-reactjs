9353509a3369d7c3429265e69ec780dbaa4dbad2
horllydreycz <oluwadaretitus3@gmail.com>
2025-11-29 06:54:16 +0100

    my-4

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
         alt="User"
         className="img rounded-full w-36 h-36 mx-auto"
       />
-      <h1 className="head text-xl text-blue-800 m">John Doe</h1>
+      <h1 className="head text-xl text-blue-800 my-4">John Doe</h1>
       <p className="paragraph text-gray-600 text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
