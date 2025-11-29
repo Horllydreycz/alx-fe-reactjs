31f5635f068fe5f9088f2476e7457c8c231bbfb6
horllydreycz <oluwadaretitus3@gmail.com>
2025-11-29 07:01:01 +0100

    screen adjustment

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4, md:p-8">
       <img
         src="https://via.plac eholder.com/150"
         alt="User"
