c82ddae04eba8b378433d09abf5b1ed1f51da36c
horllydreycz <oluwadaretitus3@gmail.com>
2025-11-29 06:46:17 +0100

    tailwind styling applied

---
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -0,0 +1,17 @@
+function UserProfile() {
+  return (
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg">
+      <img
+        src="https://via.plac eholder.com/150"
+        alt="User"
+        className="img rounded-full w-36 h-3 mx-auto"
+      />
+      <h1 className="head text-xl text-blue-800 m">John Doe</h1>
+      <p className="paragraph text-gray-600 text-base">
+        Developer at Example Co. Loves to write code and explore new
+        technologies.
+      </p>
+    </div>
+  );
+}
+export default UserProfile;
