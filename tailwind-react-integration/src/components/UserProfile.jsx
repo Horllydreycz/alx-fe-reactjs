d55ea8d7bad2aa9ea926fd441299f511d0722cfe
horllydreycz <oluwadaretitus3@gmail.com>
2025-11-29 06:51:05 +0100

    h-36

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile() {
       <img
         src="https://via.plac eholder.com/150"
         alt="User"
-        className="img rounded-full w-36 h-3 mx-auto"
+        className="img rounded-full w-36 h-36 mx-auto"
       />
       <h1 className="head text-xl text-blue-800 m">John Doe</h1>
       <p className="paragraph text-gray-600 text-base">
