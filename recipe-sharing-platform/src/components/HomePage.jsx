fad888174ed49c79a498f6036d9edefe50f517c3
horllydreycz <oluwadaretitus3@gmail.com>
2025-12-03 10:33:54 +0100

    Update HomePage component layout for responsive grid display

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -16,7 +16,7 @@ function HomePage() {
       <h1 className="head text-3xl font-bold text-center mb-10 ">
         Recipe Sharing Platform
       </h1>
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {recipes.map((recipe) => (
           <div
             key={recipe.id}
