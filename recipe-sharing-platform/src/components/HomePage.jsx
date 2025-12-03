cdd1e21e75d4b1b3a5ca38afb2d5b30e08ea1301
horllydreycz <oluwadaretitus3@gmail.com>
2025-12-03 10:08:54 +0100

    Implement HomePage component to fetch and display recipes

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,46 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-function HomePage() {}
+function HomePage() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch("src/data.json")
+      .then((response) => response.json())
+      .then((data) => setRecipes(data))
+      .catch((error) => console.error("Error loading data", error));
+  }, []);
+
+  return (
+    <div className="container bg">
+      <h1 className="head text-3xl font-bold text-center mb-10 ">
+        Recipe Sharing Platform
+      </h1>
+      <div>
+        {recipes.map((recipe) => (
+          <div
+            key={recipe.id}
+            className="bg-white shadow-lg overflow-hidden hover:scale-105"
+          >
+            <img
+              src={recipe.image}
+              alt={recipe.title}
+              className="w-full h-40 object-cover"
+            />
+            <div className="p-5">
+              <h2 className="text-xl font-semibold">{recipe.title}</h2>
+              <p className="text-grey-600 mt-2">{recipe.summary}</p>
+              <a
+                href={`/recipe/${recipe.id}`}
+                className="inline-block mt-4 text-blue-600 font-medium hover:underline"
+              >
+                View Details
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 export default HomePage;
